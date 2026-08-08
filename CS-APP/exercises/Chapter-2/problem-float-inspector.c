432d084eb1117c2be797c88a5780cba74e17891b
switchcopa <100treyholds@gmail.com>
2026-08-08 11:14:15 +0100

    Add float inspector problem for CS:APP 2.4.2 preparation

---
+++ CS-APP/exercises/Chapter-2/problem-float-inspector.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <math.h>
+
+#define FLOAT_32_ALIGNMENT sizeof(float)
+
+#define FLOAT_T_TYPE_DENORMALIZED 0
+#define FLOAT_T_TYPE_NORMALIZED   1
+#define FLOAT_T_TYPE_INFINITY     2
+#define FLOAT_T_TYPE_NAN          3
+
+struct float_t
+{
+    uint8_t  sign;
+    uint8_t  exp_raw;
+    int16_t  exp_E;
+    uint32_t fraction_f;
+    uint8_t  type;
+    uint8_t  is_aligned_4;
+};
+
+__attribute__((noinline))
+void inspect_float(const void *ptr, struct float_t *out)
+{
+    uint32_t f32;
+    memcpy(&f32, ptr, sizeof(float));
+    printf("f32 = %0X\n", f32);
+
+    out->sign = (f32 >> 31);
+    out->exp_raw = (f32 >> 23) & 0xFF;
+    out->fraction_f = (~(uint32_t)0 >> 9) & f32;
+
+    if (out->exp_raw == 255)
+    {
+        out->type  = FLOAT_T_TYPE_NAN ? 
+                     out->fraction_f != 0 : FLOAT_T_TYPE_INFINITY;
+        out->exp_E = 0;
+    }
+    else if (out->exp_raw == 0)
+    {
+        out->type  = FLOAT_T_TYPE_DENORMALIZED;
+        out->exp_E = -126;
+    }
+    else
+    {
+        out->type  = FLOAT_T_TYPE_NORMALIZED;
+        out->exp_E = out->exp_raw - 127;
+    }
+
+    out->is_aligned_4 = (intptr_t)ptr % FLOAT_32_ALIGNMENT == 0;
+}
+
+int main(void)
+{
+    float sample = -0.43879324f;
+    
+    struct float_t test_float;
+    inspect_float(&sample, &test_float);
+
+    printf("\nPointer Aligned: %s\n", test_float.is_aligned_4 ? "YES" : "NO");
+    printf("Sign:            %u\n", test_float.sign);
+    printf("Raw Exponent e:  0x%02X (%u)\n", test_float.exp_raw, test_float.exp_raw);
+    printf("Decoded Scale E: %d\n", test_float.exp_E);
+    printf("Fraction f:      0x%06X\n", test_float.fraction_f);
+    printf("Category Type:   %u \n\n", test_float.type);
+
+    return 0;
+}
