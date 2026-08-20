df472d02af376fa50e2b75bcbe0fb62efcccdb45
switchcopa <100treyholds@gmail.com>
2026-08-20 13:21:32 +0100

    CS-APP: Solve Problem 2.92

---
+++ CS-APP/exercises/Chapter-2/problem-2-92.c
@@ -0,0 +1,67 @@
+
+/* 2.92 ◆◆
+Following the bit-level floating-point coding rules, implement the function with
+the following prototype: */
+
+/* Compute -f.
+If f is NaN, then return f. */
+
+/* 
+float_bits float_negate(float_bits f);
+For floating-point number f , this function computes −f . If f is NaN, your
+function should simply return f .
+Test your function by evaluating it for all 2^32 values of argument f and com-
+paring the result to what would be obtained using your machine’s floating-point
+operations. */
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+typedef unsigned float_bits;
+
+int is_nan(float_bits f)
+{
+    unsigned char exp = (f >> 23) & 0xFF;
+    unsigned frac = f & 0x7FFFFF;
+
+    return !(~exp) && frac;
+}
+
+float_bits float_negate(float_bits f)
+{
+    if (is_nan(f))
+        return f;
+
+    f ^= 1U << 31;
+    return f;
+}
+
+float_bits float_negate_expected(float_bits f)
+{
+    float x;
+    float_bits ret;
+
+    memcpy(&x, &f, sizeof(float));
+    x = -x;
+    memcpy(&ret, &x, sizeof(float));
+
+    return ret;
+}
+
+void main(void)
+{
+    float_bits (*test_function)(float_bits);
+    float_bits (*expected)(float_bits);
+
+    test_function = float_negate;
+    expected = float_negate_expected;
+
+    for (float_bits b = 0U; b < UINT_MAX; b++)
+        if (test_function(b) != expected(b)) {
+            printf("[FAILED]: Test Suite Failed for Value %X :(\n", b);
+            return;
+        }
+
+    printf("[SUCCESS]: ALL ASSERTIONS PASSED!\n");
+}
