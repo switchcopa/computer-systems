3d095ceb6807e84872fb25c95685fe80923b3e2a
switchcopa <100treyholds@gmail.com>
2026-09-08 15:23:40 +0100

    CS-APP: Solve Problem 3-18

---
+++ CS-APP/exercises/Chapter-3/problem-3-18.c
@@ -0,0 +1,56 @@
+/*
+Practice Problem 3.18 (solution page 368)
+Starting with C code of the form
+short test(short x, short y, short z) {
+    short val = ;
+    if ( ) {
+        if ( )
+            val = ;
+        else
+            val = ;
+    } else if ( )
+        val = ;
+    return val;
+}
+
+GCC generates the following assembly code:
+short test(short x, short y, short z)
+
+x in %rdi, y in %rsi, z in %rdx
+
+test:
+    leaq (%rdx,%rsi), %rax
+    subq %rdi, %rax
+    cmpq $5, %rdx
+    jle .L2
+    cmpq $2, %rsi
+    jle .L3
+    movq %rdi, %rax
+    idivq %rdx, %rax
+    ret
+.L3:
+    movq %rdi, %rax
+    idivq %rsi, %rax
+    ret
+.L2:
+    cmpq $3, %rdx
+    jge .L4
+    movq %rdx, %rax
+    idivq %rsi, %rax
+.L4:
+    rep; ret
+
+Fill in the missing expressions in the C code.
+*/
+
+short test(short x, short y, short z) {
+    short val = y + z - x;
+    if (z > 5) {
+        if (y > 2)
+            val = x / z;
+        else
+            val = x / y;
+    } else if (z < 3)
+        val = z / y;
+    return val;
+}
